c40201efba305b0cb6588a456caafd0e4d45140e
Irbis2010 <sombalev1978@gmail.com>
2021-09-08 20:37:27 +0700

    init commit

---
+++ src/main/java/ShowBitsDemo.java
@@ -0,0 +1,36 @@
+// Пример реализации алгоритма пузырьковой сортировки
+
+class ВuЬЫе {
+    public static void main(String args[]) {
+        String strs[] = {"this", "is", "а", "test","of", "а", "string", "sort"};
+        String t;
+        int a, b;
+        int size;
+        size = strs.length; // количество сортируемьх элементов
+        // Отобразить исходный массив
+        System.out.print("Иcxoдный массив:");
+        for (int i = 0; i < size; i++)
+            System.out.print(" " + strs[i]);
+        System.out.println();
+// Реализовать алгоритм пузырьковой сортировки
+        for (a = 1; a < size; a++)
+            for (b = size - 1; b >= a; b--) {
+                if (strs[b - 1].compareTo(strs[b])>0) {
+                    t = strs[b - 1];
+                    strs[b - 1] = strs[b];
+                    strs[b] = t;
+                }
+            }
+        // если требуемый порядок
+        // следования не соблюдается,
+        // поменять элементы местами
+
+// Отобразить отсортированный массив
+        System.out.print("Oтcopтиpoвaнный массив:");
+        for (int i = 0; i < size; i++)
+            System.out.print(" " + strs[i]);
+        System.out.println();
+
+
+    }
+}
